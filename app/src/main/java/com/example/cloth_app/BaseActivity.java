f69b5b6eccde7267d10c53def7ee9ca8d4527dc7
Tharuzha <132121518+Tharuzha@users.noreply.github.com>
2024-03-25 22:23:30 +0530

    Add empty views activity class

---
+++ app/src/main/java/com/example/cloth_app/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.example.cloth_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
